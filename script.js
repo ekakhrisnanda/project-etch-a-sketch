37008fbbea1d763a0e5cff52727ffc3edc3940ba
[ekakhrisnanda] <[ekakhrisnanda@gmail.com]>
2024-12-29 18:56:45 +0800

    Create grid items from loop

---
+++ script.js
@@ -0,0 +1,8 @@
+// create grid squares
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('.container-item');
+    container.appendChild(gridItem);
+}
