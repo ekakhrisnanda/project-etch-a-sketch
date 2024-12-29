ceceb3adf6f3943c15c42a4bd85f12aff75e8b6d
[ekakhrisnanda] <[ekakhrisnanda@gmail.com]>
2024-12-29 19:24:15 +0800

    Delete period when add class

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 16; i++) {
     const gridItem = document.createElement('div');
-    gridItem.classList.add('.container-item');
+    gridItem.classList.add('container-item');
     container.appendChild(gridItem);
 }
