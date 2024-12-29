9e24fe8b08834972077c0c71250c0aba40b5c23f
[ekakhrisnanda] <[ekakhrisnanda@gmail.com]>
2024-12-29 20:04:19 +0800

    Add flex to container item and delete width in it

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create grid squares
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('container-item');
     container.appendChild(gridItem);
