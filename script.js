b0dc84fc5908118e4e2b69eb6bc275e2408a679a
Eduk <ekakhrisnanda@gmail.com>
2024-12-30 21:25:45 +0800

    Create a button for input a number
    Create a gridNum variable to store the value
    Create a function askInput to ask an input number with conditional checker
    Add event listener to the button executing askInput and change the value of gridNum to newGridNum

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // create grid squares
 const container = document.querySelector('.container');
+const inputButton = document.querySelector('.ask-input');
 
-for (let i = 0; i < 256; i++) {
+let gridNum = 16;
+
+function askInput() {
+    let newGridNum = prompt('How many do you want the grids to be?', 16);
+
+    if (newGridNum > 100 || isNaN(newGridNum)) {
+        alert('Please insert a Number');
+        return askInput();
+    } else {
+        return gridNum = newGridNum;
+    }
+}
+
+inputButton.addEventListener('click', askInput);
+
+for (let i = 0; i < gridNum * gridNum; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('container-item');
     container.appendChild(gridItem);
@@ -11,3 +27,14 @@ for (let i = 0; i < 256; i++) {
     });
 
 }
+
+// create a button from html to ask an input of how many grids they want to
+// create a function to shows the input
+// if the newGridNumbers more than 100, it shows alert to input less than that and repeat the prompt
+// else do multiplication of input x input
+// stores the input in inputNum variable
+
+// search how to make a default arguments/parameters for loop
+// create a variable and store 16 as default value
+// in for loop, change the argument to variable * variable
+// if there is a new input, change the variable value to the input newGridNumbers
